66ae2086d453ab42f867762634385fde1eabbd05
Tom Hills <cugzhangtao@163.com>
2019-05-26 18:14:52 +0800

    首页增加分页

--- django/blog/post/views.py
+++ django/blog/post/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from post import models
 
 def index(request):
-    posts = models.Post.published.all()
+    post_list = models.Post.published.all()
+    paginator = Paginator(post_list, 2)
+    page = request.GET.get("page")
+    posts = paginator.get_page(page)
     return render(request, 'post/index.html', {'posts': posts})
